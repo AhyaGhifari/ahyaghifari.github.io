0b7a05730418fef4f8e9cfd934030c802dfd1152
ahyaghifari <ahyaghifari288@gmail.com>
2023-03-12 22:19:59 +0800

    update new nature theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
     $('title').html('Home | Ahya Ghifari');
     $("main").load("home.html");
-
+    
     $('.nav').click(function () {
         $('#main-transition').addClass('fade-in');
         var nav = $(this).data('nav');
+        $('title').html(nav[0].toUpperCase() + nav.slice(1) + ' | Ahya Ghifari');
         $('#main-transition').html(nav);
         $('.nav').removeClass('active');
         $(this).addClass('active');
